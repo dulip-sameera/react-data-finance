55b347bbba8e28147a45b208dbc7b75256cdb018
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-19 06:24:13 +0530

    move up the comment line

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,12 @@ const Navbar = () => {
             <li className='p-4'>About</li>
             <li className='p-4'>Contact</li>
         </ul>
+        
+        {/* Mobile Menu */}
         <div onClick={handleNav} className="block hover:cursor-pointer md:hidden">
             {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             
         </div>
-        {/* Mobile Menu */}
         <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' }>
             <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
             <ul className='p-4 uppercase'>
